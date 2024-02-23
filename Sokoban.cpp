d376c5614aabdd689afa43a405b55dce323996d2
NoseCat <zhirnovvladislav99@gmail.com>
2024-02-23 19:15:48 +0300

    как то так это должно выглядеть

--- Sokoban.cpp
+++ Sokoban.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
+#include <conio.h>
+
+#include "Menu.h"
+#include "Movement.h"
+
+extern Matrix[]
 
 int main()
 {
-	printf("Sokoban");
+	system("chcp 1251 > 0");
+	while (win_check())
+	{
+		display();
+
+		dir = get_player_input();
+		move_player(dir);
+	}
 }
