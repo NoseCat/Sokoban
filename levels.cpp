e1bed0497a6d5cd05a75f55a12fa4c143d2f2256
Никита Однобурцев <nodnoburcev@gmail.com>
2024-02-26 17:22:33 +0300

    nu tipo

--- levels.cpp
+++ levels.cpp
@@ -5,11 +5,12 @@
 
 enum objects { EMPTY = 1, WALL, PLAYER, BOX, FINISH, NEXTROWS};
 
-enum sizes { rows = 20, cols = 20 };
+enum sizes { rows = 20, cols = 20, razmaszh=100};
 
 //const int cols = 20, rows = 20;
 int mas[rows][cols];
 
+int masznach_x[razmaszh];
 void openfilelvl(char* fullstr)
 {
 	FILE* level;
@@ -24,28 +25,46 @@ void openfilelvl(char* fullstr)
 void filmas(char str[])
 {
 	int counterstr = -1;
+	masznach_x[razmaszh] = {};
+	int mas_x = 0;
 	for (int i = 0; i < rows; i++)
 	{
 		bool flag = false;
 		for (int j = 0; j < cols; j++)
 		{
 			counterstr++;
-			if (str[counterstr] == '#')
-				mas[i][j] = WALL;
-			else if (str[counterstr] == '.' or str[counterstr] == ' ')
-				mas[i][j] = EMPTY;
-			else if (str[counterstr] == '@')
-				mas[i][j] = PLAYER;
-			else if (str[counterstr] == 'B')
-				mas[i][j] = BOX;
-			else if (str[counterstr] == 'X')
-				mas[i][j] = FINISH;
-			else if (str[counterstr] == '\n')
+			switch (str[counterstr])
 			{
+			case '#':
+				mas[i][j] = WALL;
+				break;
+			case '.':
+			case ' ':
+				mas[i][j] = EMPTY;
+				break;
+			case '@':
+				mas[i][j] = PLAYER;
+				break;
+			case 'B':
+				mas[i][j] = BOX;
+				break;
+			case 'X':
+				masznach_x[mas_x] = i;
+				masznach_x[mas_x+1] = j;
+				mas_x += 2;
+				mas[i][j] = FINISH;
+
+				break;
+			case'\n':
+			
 				mas[i][j] = NEXTROWS;
 				flag = true;
+			
+			default:
 				break;
 			}
+			if (flag)
+				break;
 		}
 		if (!flag)
 			break;
@@ -58,6 +77,39 @@ void displaymatrix()
 	{
 		for (int j = 0; j < cols; j++)
 		{
+			switch (mas[i][j])
+			{
+			case WALL:
+				printf("#");
+				break;
+			case EMPTY:
+				bool fulage = false;
+				for (int k = 0; k < razmaszh - 1; k+=2)
+				{
+					if (i == masznach_x[k] and j == masznach_x[k + 1])
+						fulage = true;
+					if (0 == masznach_x[k] and 0 == masznach_x[k + 1])
+						break;
+				}
+				if(fulage)
+					printf("X");
+				else
+					printf(" ");
+				break;
+			case PLAYER:
+				printf("@");
+				break;
+			case BOX:
+				printf("B");
+				break;
+			case FINISH:
+				printf("X");
+				break;
+			case NEXTROWS:
+				printf("\n");
+				break;
+			}
+			/*
 			if (mas[i][j] == WALL)
 				printf("#");
 			else if (mas[i][j] == EMPTY)
@@ -69,7 +121,7 @@ void displaymatrix()
 			else if (mas[i][j] == FINISH)
 				printf("X");
 			else if (mas[i][j] == NEXTROWS)
-				printf("\n");
+				printf("\n");*/
 		}
 	}
 }
