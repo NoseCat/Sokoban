e5d3529124c2701968ec959b589042299bef69ea
Никита Однобурцев <nodnoburcev@gmail.com>
2024-03-01 13:45:56 +0300

    /{HUsdass/

--- levels.cpp
+++ levels.cpp
@@ -1,16 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
-
+#include <windows.h>
 #include <iostream>
 #include <string.h>
 
-enum objects { EMPTY = 1, WALL, PLAYER, BOX, FINISH, NEXTROWS };
-
+enum objects { EMPTY = 1, WALL, PLAYER, WHITEBOX, REDBOX, GREENBOX, BLUEBOX, FINISH, NEXTROWS };
+enum color { white = 7,red=12,green=10,blue=9 };
 enum maxsizes { rows = 64, cols = 64, razmaszh = 16 *3};
 //максимальное количество крестов на поле = razmaszh / 3
 
 //const int cols = 20, rows = 20;
 int mas[rows][cols];
-
+int znach_x = 3;
 int masznach_x[razmaszh];
 void openfilelvl(char* fullstr, const char lvl_name[])
 {
@@ -48,13 +48,41 @@ void filmas(char str[], int mass[][cols])
 			case '@':
 				mass[i][j] = PLAYER;
 				break;
-			case 'B':
-				mass[i][j] = BOX;
+			case 'w':
+				mass[i][j] = WHITEBOX;
 				break;
-			case 'X':
+			case 'r':
+				mass[i][j] = REDBOX;
+				break;
+			case 'g':
+				mass[i][j] = GREENBOX;
+				break;
+			case 'b':
+				mass[i][j] = BLUEBOX;
+				break;
+			case 'W':
+			case 'R':
+			case 'G':
+			case 'B':
+				switch (str[counterstr])
+				{
+				case 'W':
+					masznach_x[mas_x + 2] = white;
+					break;
+				case 'R':
+					masznach_x[mas_x + 2] = red;
+					break;
+				case 'G':
+					masznach_x[mas_x + 2] = green;
+					break;
+				case 'B':
+					masznach_x[mas_x + 2] = blue;
+					break;
+				}
 				masznach_x[mas_x] = i;
 				masznach_x[mas_x + 1] = j;
-				mas_x += 2;
+				
+				mas_x += znach_x;
 				mass[i][j] = EMPTY;
 
 				break;
@@ -76,12 +104,14 @@ void filmas(char str[], int mass[][cols])
 
 void displaymatrix()
 {
+	HANDLE hwnd = GetStdHandle(STD_OUTPUT_HANDLE);
 	bool fulage;
+	int color_x = 2;
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			bool fulage;
+			
 			switch (mas[i][j])
 			{
 			case WALL:
@@ -89,26 +119,51 @@ void displaymatrix()
 				break;
 			case EMPTY:
 				fulage = false;
-				for (int k = 0; k < razmaszh - 1; k += 2)
+				for (int k = 0; k < razmaszh - 2; k += znach_x)
 				{
 					if (0 == masznach_x[k] and 0 == masznach_x[k + 1])
 						break;
 					if (i == masznach_x[k] and j == masznach_x[k + 1])
+					{
 						fulage = true;
+
+					}
 				}
 				if (fulage)
+				{
+					SetConsoleTextAttribute(hwnd, masznach_x[color_x]);
 					printf("X");
+					SetConsoleTextAttribute(hwnd, white);
+					color_x += znach_x;
+				}
 				else
 					printf(" ");
 				break;
 			case PLAYER:
 				printf("@");
 				break;
-			case BOX:
+			case WHITEBOX:
+				
 				printf("B");
+				break;
+			case REDBOX:
+				SetConsoleTextAttribute(hwnd, red);
+				printf("B");
+				SetConsoleTextAttribute(hwnd, white);
+				break;
+			case GREENBOX:
+				SetConsoleTextAttribute(hwnd, green);
+				printf("B");
+				SetConsoleTextAttribute(hwnd, white);
+				break;
+			case BLUEBOX:
+				SetConsoleTextAttribute(hwnd, blue);
+				printf("B");
+				SetConsoleTextAttribute(hwnd, white);
 				break;
 			case FINISH:
 				printf("X");
+
 				break;
 			case NEXTROWS:
 				printf("\n");
@@ -116,4 +171,5 @@ void displaymatrix()
 			}
 		}
 	}
+	
 }
